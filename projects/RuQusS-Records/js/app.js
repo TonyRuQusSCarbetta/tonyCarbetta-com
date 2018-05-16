c03da4ec72af2b217452523eb32b3bc74d846c4f
Tony Carbetta <tony@Tonys-MacBook-Pro.local>
2018-05-15 22:37:07 -0700

    done

--- projects/RuQusS-Records/js/app.js
+++ projects/RuQusS-Records/js/app.js
@@ -1,3 +1,24 @@
 $("#menuButton").click(function() {
-  $("nav").toggleClass("animated slideOutRight");
+  $("nav").toggleClass("animated slideOutLeft");
+});
+
+
+$("#artistDev").click(function() {
+  $("nav").toggleClass("animated slideOutLeft");
+});
+
+$("#musicProd").click(function() {
+  $("nav").toggleClass("animated slideOutLeft");
+});
+
+$("#webServ").click(function() {
+  $("nav").toggleClass("animated slideOutLeft");
+});
+
+$("#cont").click(function() {
+  $("nav").toggleClass("animated slideOutLeft");
+});
+
+$("#menuTitle").click(function() {
+  $("nav").toggleClass("animated slideOutLeft");
 });
